3105eb160ead88d8aaa203f33c49538ef56bcced
Xkiller8game <y.jaishreyas@gmail.com>
2023-11-01 01:23:59 +0530

    created distance function

--- ex.c
+++ ex.c
@@ -8,8 +8,21 @@ typedef struct kdt
     int *data;
     struct kdt *left,*right;
 }kdt;
+int k=-1;
 
 
-int main(){
-    
+
+double distance_parameter(int *arr1 , int *arr2)
+{
+    double interspace = 0;
+    int i;
+    for (i=0;i<k;i++)
+        interspace += (arr1[i] - arr2[i])*(arr1[i] - arr2[i]);
+    return interspace;
+}
+
+
+int main()
+{
+    return 0;    
 }
